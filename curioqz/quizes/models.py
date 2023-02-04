3decd715c26a3a1155293d23418e0732c1e6e1da
yusufadell <yusufadell.dev@gmail.com>
2023-02-04 21:38:46 +0200

    register courses app amd added its migration file

--- curioqz/quizes/models.py
+++ curioqz/quizes/models.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -6,36 +8,25 @@ User = get_user_model()
 
 
 class Quiz(models.Model):
-    # TODO: link with courses app
     class QuizChoices(models.TextChoices):
         EASY = "easy", "easy"
         MEDIUM = "medium", "medium"
         HARD = "hard", "hard"
 
     name = models.CharField(max_length=255)
+    topic = models.CharField(max_length=255)
     intro = models.TextField(blank=True, null=True)
     introformat = models.CharField(max_length=10, default="html")
-    time_open = models.DateTimeField(blank=True, null=True)
-    time_close = models.DateTimeField(blank=True, null=True)
-    attempts = models.IntegerField(default=0)
-    attempt_on_last = models.BooleanField(default=False)
-    grade_method = models.CharField(max_length=10, default="highest")
-    decimal_points = models.DecimalField(max_digits=3, decimal_places=0)
-    review_attempt = models.IntegerField(default=0)
-    review_correctness = models.IntegerField(default=0)
-    review_marks = models.IntegerField(default=0)
-    review_specific_feedback = models.IntegerField(default=0)
-    review_general_feedback = models.IntegerField(default=0)
-    review_right_answer = models.IntegerField(default=0)
-    review_overall_feedback = models.IntegerField(default=0)
-    question_per_page = models.IntegerField(default=0)
-    shuffle_questions = models.BooleanField(default=False)
-    shuffle_answers = models.BooleanField(default=False)
-    # TODO: add FK to Grade, and QuizAttempt models
-    topic = models.CharField(max_length=255)
+
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    time_open = models.DateTimeField(blank=True, null=True)
+    time_close = models.DateTimeField(blank=True, null=True)
     time_limit = models.IntegerField(help_text=_("time in minutes"))
+
+    grade_method = models.CharField(max_length=10, default="highest")
+    decimal_points = models.DecimalField(max_digits=3, decimal_places=0)
+    question_per_page = models.IntegerField(default=0)
 
     difficulty = models.CharField(max_length=6, choices=QuizChoices.choices)
     score_to_pass = models.IntegerField(
@@ -43,21 +34,97 @@ class Quiz(models.Model):
             "required score to pass the quiz",
         )
     )
-    delay1 = models.IntegerField(
-        help_text=_(
-            "delay between attempts in minutes",
-        )
-    )
+    delay1 = models.IntegerField()
+    delay2 = models.IntegerField()
 
-    delay2 = models.IntegerField(
-        help_text=_(
-            "delay between attempts in minutes",
-        )
-    )
     show_user_picture = models.BooleanField(default=False)
     show_blocks = models.BooleanField(default=False)
+    shuffle_questions = models.BooleanField(default=False)
+    shuffle_answers = models.BooleanField(default=False)
 
-    questions = models.ManyToManyField("Question")
+    course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
+    review = models.ForeignKey("Review", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name} - {self.topic}"
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=255)
+    marks = models.IntegerField(default=0)
+    right_answer = models.IntegerField(default=0)
+    correctness = models.IntegerField(default=0)
+    specific_feedback = models.TextField(blank=True, null=True)
+    general_feedback = models.TextField(blank=True, null=True)
+    overall_feedback = models.TextField(blank=True, null=True)
+
+    reviewer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+    )
+
+    def __str__(self):
+        return f"{self.title} - {self.marks}"
+
+
+class Attempt(models.Model):
+    slug = models.SlugField(default=uuid4, unique=True)
+
+    start = models.DateTimeField(auto_now=True)
+    finish = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    preview = models.IntegerField(default=0)
+
+    quiz = models.ForeignKey(
+        "Quiz",
+        on_delete=models.CASCADE,
+        related_name="attempts",
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="attempts",
+    )
+
+
+class Grade(models.Model):
+    grade = models.DecimalField(max_digits=3, decimal_places=0)
+
+
+class QuizGrade(models.Model):
+    quiz = models.ForeignKey(
+        "Quiz",
+        on_delete=models.CASCADE,
+        related_name="quiz_grades",
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="quiz_grades",
+    )
+    grade = models.ForeignKey(
+        "Grade",
+        on_delete=models.CASCADE,
+        related_name="quiz_grades",
+    )
+
+    modified = models.DateTimeField(auto_now=True)
+
+
+class QuizQuestion(models.Model):
+    quiz = models.ForeignKey(
+        "Quiz",
+        on_delete=models.CASCADE,
+        related_name="quiz_questions",
+    )
+
+    questions = models.ForeignKey(
+        "questions.Question",
+        on_delete=models.CASCADE,
+        related_name="quiz_questions",
+    )
+
+
+# TODO: add quiz reports
