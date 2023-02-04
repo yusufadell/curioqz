b1d129c9f03441f76eb4719673ec90017739083c
yusufadell <yusufadell.dev@gmail.com>
2023-02-04 15:18:11 +0200

    feat: denormalize quizes app

--- curioqz/quizes/models.py
+++ curioqz/quizes/models.py
@@ -12,77 +12,52 @@ class Quiz(models.Model):
         MEDIUM = "medium", "medium"
         HARD = "hard", "hard"
 
-    name = models.CharField(max_length=200)
-    topic = models.CharField(max_length=200)
+    name = models.CharField(max_length=255)
+    intro = models.TextField(blank=True, null=True)
+    introformat = models.CharField(max_length=10, default="html")
+    time_open = models.DateTimeField(blank=True, null=True)
+    time_close = models.DateTimeField(blank=True, null=True)
+    attempts = models.IntegerField(default=0)
+    attempt_on_last = models.BooleanField(default=False)
+    grade_method = models.CharField(max_length=10, default="highest")
+    decimal_points = models.DecimalField(max_digits=3, decimal_places=0)
+    review_attempt = models.IntegerField(default=0)
+    review_correctness = models.IntegerField(default=0)
+    review_marks = models.IntegerField(default=0)
+    review_specific_feedback = models.IntegerField(default=0)
+    review_general_feedback = models.IntegerField(default=0)
+    review_right_answer = models.IntegerField(default=0)
+    review_overall_feedback = models.IntegerField(default=0)
+    question_per_page = models.IntegerField(default=0)
+    shuffle_questions = models.BooleanField(default=False)
+    shuffle_answers = models.BooleanField(default=False)
+    # TODO: add FK to Grade, and QuizAttempt models
+    topic = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
-    time = models.IntegerField(help_text=_("time in minutes"))
+    modified = models.DateTimeField(auto_now=True)
+    time_limit = models.IntegerField(help_text=_("time in minutes"))
+
     difficulty = models.CharField(max_length=6, choices=QuizChoices.choices)
     score_to_pass = models.IntegerField(
         help_text=_(
             "required score to pass the quiz",
         )
     )
+    delay1 = models.IntegerField(
+        help_text=_(
+            "delay between attempts in minutes",
+        )
+    )
+
+    delay2 = models.IntegerField(
+        help_text=_(
+            "delay between attempts in minutes",
+        )
+    )
+    show_user_picture = models.BooleanField(default=False)
+    show_blocks = models.BooleanField(default=False)
 
     questions = models.ManyToManyField("Question")
 
     def __str__(self):
         return f"{self.name} - {self.topic}"
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
-    info = models.TextField(blank=True, null=True)
-    order = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.name
-
-
-class Question(models.Model):
-    # TODO: add question app
-    class QuestionChoices(models.TextChoices):
-        MULTIPLE_CHOICE = "multiple-choice", "multiple-choice"
-        TRUE_FALSE = "true-false", "true-false"
-        FILL_IN_THE_BLANK = "fill-in-the-blank", "fill-in-the-blank"
-
-    text = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="images/", blank=True, null=True)
-    qtype = models.CharField(max_length=13, choices=QuestionChoices.choices)
-    created = models.DateTimeField(auto_now_add=True)
-    hidden = models.BooleanField(default=False)
-
-    created_by = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="questions",
-    )
-
-    quiz = models.ForeignKey(
-        "Quiz",
-        on_delete=models.CASCADE,
-        related_name="questions",
-    )
-    category = models.ForeignKey(
-        "Category",
-        on_delete=models.CASCADE,
-        related_name="questions",
-        blank=True,
-        null=True,
-    )
-
-    def __str__(self):
-        return str(self.text)
-
-
-class Answer(models.Model):
-    text = models.CharField(max_length=200)
-    correct = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
-    feedback = models.TextField(blank=True, null=True)
-
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    questions = models.ManyToManyField("Question")
-
-    def __str__(self):
-        return f"question: {self.question.text}, answer: {self.text}, correct: {self.correct}"
