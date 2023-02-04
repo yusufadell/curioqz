f43506ae876a95f4dc75140b934fe94baea7b801
yusufadell <yusufadell.dev@gmail.com>
2023-02-04 21:45:44 +0200

    add plural name for quizes in admin panel

--- curioqz/quizes/models.py
+++ curioqz/quizes/models.py
@@ -45,6 +45,9 @@ class Quiz(models.Model):
     course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
     review = models.ForeignKey("Review", on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name_plural = _("quizes")
+
     def __str__(self):
         return f"{self.name} - {self.topic}"
 
