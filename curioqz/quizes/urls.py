61ce536624256541d55fe44c5bc8a99e1e978589
yusufadell <yusufadell.dev@gmail.com>
2023-02-06 00:38:41 +0200

    feat(quizes): register quizes viewsets in urlpatterns

--- curioqz/quizes/urls.py
+++ curioqz/quizes/urls.py
@@ -1,4 +1,15 @@
-from django.urls import path
+from django.conf import settings
+from rest_framework.routers import DefaultRouter, SimpleRouter
+
+from curioqz.quizes.api.views import QuizGradeViewSet, QuizViewSet
+
+if settings.DEBUG:
+    router = DefaultRouter()
+else:
+    router = SimpleRouter()
+
+router.register("quizes", QuizViewSet)
+router.register("quiz-grades", QuizGradeViewSet)
 
 app_name = "quizes"
-urlpatterns = []
+urlpatterns = router.urls
